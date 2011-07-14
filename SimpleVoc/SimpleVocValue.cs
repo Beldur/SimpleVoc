b6543bae6f9d1fe9b7d9652d4912847bfd3870c1
Beldur <alexander@alexander-luecking.de>
2011-07-14 20:53:25 +0200

    Initial commit

---
+++ SimpleVoc/SimpleVocValue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SimpleVoc
+{
+    public struct SimpleVocValue
+    {
+        public DateTime Created { get; set; }
+        public DateTime Expires { get; set; }
+        public string Flags { get; set; }
+        public string Data { get; set; }
+        public string Key { get; set; }
+    }
+}
